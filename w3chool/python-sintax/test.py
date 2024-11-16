dd98506bb899a12016e100039c55e0055200e051
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-16 17:18:24 +0600

    first commit

---
+++ w3chool/python-sintax/test.py
@@ -0,0 +1,21 @@
+# first code of python
+print("Hello, World!")
+
+# variable and data types
+x = 10 # Integer
+y = 3.14 # Float
+name= "Alice" # string
+is_active = True # Boolean
+print(x,y,name,is_active)
+
+# Basic Data structure 
+# Lists: Ordered, mutable sequences.
+my_list = [1,2,3,4,5]
+# Tuples: Ordered, immutable sequences
+my_tuple= (1,2,3,4,5)
+# Dictionaries: Key-value pairs, mutable.
+my_dict = {'name': 'Alice', 'age': 25}
+# Sets: Unordered collections of unique elements.
+my_set = {1, 2, 3}
+
+print(my_list, my_tuple, my_dict, my_set)
